1af2017ab6d8a9971f29a7f7e59258b620458651
yhcho14 <mocha.cyh@gmail.com>
2021-11-02 14:30:18 +0900

    Add Wall
    벽 추가

--- TeamProject/Assets/Scripts/ButtonController.cs
+++ TeamProject/Assets/Scripts/ButtonController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class ButtonController : MonoBehaviour
 {
     [SerializeField] private GameObject ButtonTop;
-    [SerializeField] private float ButtonDepth;
-    [SerializeField] private LayerMask WhatisButton;
+    [SerializeField] private GameObject Wall;
+    [SerializeField] private LayerMask WhatIsButton;
     [SerializeField] private Transform feetPos;
     [SerializeField] private float checkRadius;
+    [SerializeField] private float ButtonDepth;
+
 
     private bool isTouched;
     private bool isButtonTop;
@@ -20,19 +22,29 @@ public class ButtonController : MonoBehaviour
 
     void Update()
     {
-        isTouched = Physics2D.OverlapCircle(feetPos.position, checkRadius, WhatisButton);
+        isTouched = Physics2D.OverlapCircle(feetPos.position, checkRadius, WhatIsButton);
 
         if(isTouched && (isButtonTop == true))
         {
             ButtonTop.transform.Translate(0, -(ButtonDepth), 0);
             isButtonTop = false;
             //버튼 눌림
-
         }
         else if((!isTouched) && (isButtonTop == false))
         {
             ButtonTop.transform.Translate(0, ButtonDepth, 0);
             isButtonTop = true;
+        }
+
+        if(isButtonTop == false)
+        {
+            if(Wall.transform.position.y > -10)
+                Wall.transform.Translate(0, -0.1f, 0);
+        }
+        else
+        {
+            if (Wall.transform.position.y < 0)
+                Wall.transform.Translate(0, 0.2f, 0);
         }
     }
 }
